0f027c76c432c8914f12bf9adb2dea4f0883c175
mariem-jpg <mariemgribi804@gmail.com*>
2026-05-17 20:13:32 +0100

    souad6

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('je t aime infinement');
+    res.send('mariem ghribi');
 });
 
 app.listen(3001, () => {
