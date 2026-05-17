894c2f767cf0a23f66cdcd531fda57af8e755c09
mariem-jpg <mariemgribi804@gmail.com*>
2026-05-17 18:31:40 +0100

    Changer message en Bonjour

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
-  res.send('bonjour');
+  res.send('okkkk');
 });
 app.listen(3001, () => {
   console.log('Server sur port 3001');
