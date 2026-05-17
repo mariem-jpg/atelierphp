353e9e7df2d1da756d1545ccc2f468e5779faf22
mariem-jpg <mariemgribi804@gmail.com*>
2026-05-17 11:48:05 +0100

    Déplacement des fichiers dans docker-node-app

--- app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello Docker !');
-});
-app.listen(3001, () => {
-  console.log('Server sur port 3001');
-});
