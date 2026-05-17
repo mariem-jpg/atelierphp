18f1e4415a653575416bc18206562818b0c268c3
mariem-jpg <mariemgribi804@gmail.com*>
2026-05-17 18:13:54 +0100

    changt

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Docker !');
+  res.send('bonjour');
 });
 app.listen(3001, () => {
   console.log('Server sur port 3001');
