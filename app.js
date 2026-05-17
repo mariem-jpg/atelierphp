18eb1b2815aaeac03f34e587211acb43b128f432
mariem-jpg <mariemgribi804@gmail.com*>
2026-05-17 18:46:19 +0100

    Bonsoir à tous

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
-  res.send('okkkk');
+  res.send('bonsoir a tous');
 });
 app.listen(3001, () => {
   console.log('Server sur port 3001');
