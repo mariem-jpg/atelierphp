7df0c3fcaf8d321a203b924e45f9ab4ab0d2bd62
mariem-jpg <mariemgribi804@gmail.com*>
2026-05-17 19:37:36 +0100

    mc

--- docker-node-app/app.js
+++ docker-node-app/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('je suis la ');
+    res.send('cc bb cv  ');
 });
 
 app.listen(3001, () => {
