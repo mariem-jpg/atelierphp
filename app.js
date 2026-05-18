f396b2bf2608b7b83c37c7b519f04b32c9883ed1
mariem-jpg <mariemgribi804@gmail.com*>
2026-05-18 14:32:57 +0100

    hellomaryem

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('hello ranya ');
+    res.send('hello maryem');
 });
 
 app.listen(3001, () => {
