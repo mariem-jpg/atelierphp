a7101727a5288931bb340398ab6780f9376c5b4b
mariem-jpg <mariemgribi804@gmail.com*>
2026-05-18 08:42:57 +0100

    dsi

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('bonjour mariem ghribi');
+    res.send('bonjour mariem ghribi dsi 22');
 });
 
 app.listen(3001, () => {
